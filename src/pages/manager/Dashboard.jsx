aa9959996713137f78c3bd8b81e7778ff7447df8
DJone-digi2025 <davidjones.digi.2025@gmail.com>
2026-04-13 13:01:29 +0530

    tasks page rework filter updated2

--- src/pages/manager/Dashboard.jsx
+++ src/pages/manager/Dashboard.jsx
@@ -545,7 +545,7 @@ return (
   <option value="">All Status</option>
   <option value="ASSIGNED">Assigned</option>
   <option value="SUBMITTED">Submitted</option>
-  <option value="COMPLETED">Rework</option>
+  <option value="REWORK">Rework</option>
 </select>
 
 <select value={stageFilter} onChange={(e) => setStageFilter(e.target.value)}>
